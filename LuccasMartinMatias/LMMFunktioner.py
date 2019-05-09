e8b254d237f0f6106fe4e9a8cac1daf488eafabe
MatiasH <matiasholst787@gmail.com>
2019-05-09 14:18:34 +0200

    Ups

--- LuccasMartinMatias/LMMFunktioner.py
+++ LuccasMartinMatias/LMMFunktioner.py
@@ -8,7 +8,7 @@ def FindMidtPunkt (x1,y1,x2,y2):
     return x,y
 
 def findAfstand (x1, y1, x2, y2):
-    afstand = math.sqrt((x1+x2)*(x1+x2)+(y1+y2)*(y1+y2))
+    afstand = math.sqrt((x1 - x2)*(x1 - x2)+(y1 - y2)*(y1 - y2))
 
     return afstand
 
