dc4c8961b1dc5e1435e72623d230a2bded14c5e1
BuildTools <unconfigured@null.spigotmc.org>
2019-05-09 12:25:53 +0200

    Lavet en masse filer og tilføjede funktioner n stuff

---
+++ Samuel-Simon-Sophus/historie(r).py
@@ -0,0 +1,14 @@
+# Historie(rne)
+import random
+
+def historie1():
+    print("...")
+
+    svar = picker1()
+    if (svar == 1):
+        print("")
+
+    
+
+def historie2():
+    print("...")
--- Samuel-Simon-Sophus/main.py
+++ Samuel-Simon-Sophus/main.py
@@ -1,1 +1,8 @@
-#SCOR EN DAME
+#SCOR EN DAME SPIL
+import random
+
+def spil():
+    picker1()
+    picker2()
+    picker3()
+    picker4()
