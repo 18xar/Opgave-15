39ea0a5ecc4c154728d97f5e5822ce0ff9640b16
oskarkragh <kraghoskar@gmail.com>
2019-05-09 13:51:34 +0200

    arcade kordinat virker

--- OliverOskarOtto/render.py
+++ OliverOskarOtto/render.py
@@ -1,21 +1,20 @@
 import arcade
 
-width = 800
-height = 800
-lineWidth = 5
+def TriangeRender(Ax, Ay, Bx, By, Cx, Cy):
+    width = 800
+    height = 800
+    lineWidth = 5
 
-arcade.open_window(width, height, "Triagle Cordinate Drawer")
-arcade.set_background_color((255, 255, 255))
+    arcade.open_window(width, height, "Triagle Cordinate Drawer")
+    arcade.set_background_color((255, 255, 255))
 
-Ax = 1
-Ay = 2
-Bx = 6
-By = 3
-Cx = 3
-Cy = 10
+    #Draw axes
+    arcade.draw_line(width/2, 0, width/2, height, (255, 255, 255), lineWidth)
+    arcade.draw_line(0, height/2, width, height/2, (255, 255, 255), lineWidth)
+    #draw point
+    arcade.draw_point(Ax,Ay,(255, 180, 180), 20)
 
-#Draw axes
-arcade.draw_line(width/2, 0, width/2, height, (0, 0, 0), lineWidth)
+    arcade.finish_render()
+    arcade.run()
 
-arcade.finish_render()
-arcade.run()
+TriangeRender(300,300,400,400,300,600)
