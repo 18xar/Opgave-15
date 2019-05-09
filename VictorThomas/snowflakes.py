7f32553bbb7046f1f4244eb80fa0ee404cf1becc
Thomas <thoaskson@gmail.com>
2019-05-09 14:17:46 +0200

    Basic design for Snowflake

--- VictorThomas/snowflakes.py
+++ VictorThomas/snowflakes.py
@@ -1,20 +1,25 @@
 import random, arcade
 
 def snowflake (x, y):
-    size1 = 30
-    size2 = 30
+
+    s = random.randint(5, 20)
+    size = random.randint(2, 3)
+    nLeaf = random.randint(1, 2)
+
+    arcade.draw_line(30 + x, s + y, 30 + x, 60 - s + y, (255, 255, 255), size)
+    arcade.draw_line(s + x, 30 + y, 60 - s + x, 30 + y, (255, 255, 255), size)
+
+    if (nLeaf == 2):
+        arcade.draw_line(s + 5 + x, 60 - s - 5 + y, 60 - s - 5 + x, s + 5 + y, (255, 255, 255), size)
+        arcade.draw_line(60 - s - 5 + x, 60 - s - 5 + y, s + 5 + x, s + 5 + y, (255, 255, 255), size)
 
 
-    arcade.draw_line(30 + x, 10 + y, 30 + x, 50 + y, (255, 255, 255), 2)
 
 
-
-
-
-arcade.open_window(160,160, "Snowflake")
+arcade.open_window(60,60, "Snowflake")
 arcade.set_background_color((0, 0, 0))
 
-snowflake()
+snowflake(0, 0)
 
 arcade.finish_render()
 arcade.run()
