648aae0ed4a3f1da7a7d4ebe5e70bc8a60fd52c1
oskarkragh <kraghoskar@gmail.com>
2019-05-09 13:10:57 +0200

    Cos and Sin works Pints done

--- OliverOskarOtto/PointsToDistance.py
+++ OliverOskarOtto/PointsToDistance.py
@@ -3,4 +3,6 @@ def PointsToDistance(Ax, Ay, Bx, By, Cx, Cy)
 
     AB=math.sqrt((Ax-Bx)*(Ax-Bx)+(Ay-By)*(Ay-By))
     AC=math.sqrt((Ax-Cx)*(Ax-Cx)+(Ay-Cy)*(Ay-Cy))
+    BC=math.sqrt((Bx-Cx)*(Bx-Cx)+(By-Cy)*(By-Cy))
 
+    return(AB, AC, BC)
