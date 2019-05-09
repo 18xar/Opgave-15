2b1b6fc027b40904a7c6a49f30f2b0865a0e2424
OttoJHTX <sejeotto@gmail.com>
2019-05-09 13:12:01 +0200

    GUI lavet til en funktion

--- OliverOskarOtto/gui.py
+++ OliverOskarOtto/gui.py
@@ -1,14 +1,22 @@
-import PySimpleGUI as sg
+def gui():
+    import PySimpleGUI as sg
 
-layout = [[sg.Text("Point A:")],
-    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
-    [sg.Text("Point B:")],
-    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
-    [sg.Text("Point C:")],
-    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
-    [sg.Submit()]
-]
+    layout = [[sg.Text("Point A:")],
+        [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+        [sg.Text("Point B:")],
+        [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+        [sg.Text("Point C:")],
+        [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+        [sg.Submit()]
+    ]
 
-done = sg.Window("Put in coordinates").Layout(layout)
-button, values = done.Read()
+    done = sg.Window("Put in coordinates").Layout(layout)
+    button, values = done.Read()
 
+    ax = float(values[0])
+    ay = float(values[1])
+    bx = float(values[2])
+    by = float(values[3])
+    cx = float(values[4])
+    cy = float(values[5])
+    return ax, ay, bx, by, cx, cy
