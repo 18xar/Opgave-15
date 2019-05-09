2267a6fc1d64b52f6957451721f6ddf4b5937d94
BuildTools <unconfigured@null.spigotmc.org>
2019-05-09 13:37:11 +0200

    gjorde "på baren" færdig

--- Samuel-Simon-Sophus/paa-baren.py
+++ Samuel-Simon-Sophus/paa-baren.py
@@ -11,10 +11,18 @@ def paaBaren():
     if (svar1 == "1"):
         print("Hun ignorer dig...og går. (du kan ikke forsøge igen, hun har mistet interessen)")
 
-        rape = input("Vil du rape hende? (ja eller nej): ")
+        rape = input("Vil du rape hende? (1: \"ja\" eller 2: \"nej\"): ")
 
-        if (rape == "ja" or rape == "Ja"):
+        if (rape == "1"):
             print("Du finder hende, raper hende og ryger i fængsel kort efter")
         else:
             print("Du raper hende ikke (det var hun sikkert glad for), men da du vælger at nuppe en meget stærk drink efter nederlaget, ryger du direkte i gulvet og dør!")
 
+    if (svar1 == "2"):
+        print("Du tager hende på røven og får lov til, at nyde det i et helt sekundt, før du får en kæmpe knytnæve og går i jorden")
+
+    if (svar1 == "3"):
+        print("""
+Hun smiler, tager imod drinken og i begynder at snakke meget godt.
+Lidt tid går og hun kan rigtigt godt lide dig hun tager dig med hjem til hende.
+""")
