426162d2e06d3cca7c04c41800e97971ca0f064d
Wilfred <wilfred.lasthein@gmail.com>
2019-05-16 14:15:03 +0200

    lommeregner

---
+++ WilliamWilfred/LommeregnerFunktioner.py
@@ -0,0 +1,37 @@
+import math
+
+def minus(a,b):
+    resultat = a - b
+    return resultat
+
+def plus(a,b):
+    resultat = a + b
+    return resultat
+
+def gange(a,b):
+    resultat = a * b
+    return resultat
+
+def division(a,b):
+    resultat = a / b
+    return resultat
+
+def kvadratrod(a):
+    resultat = math.sqrt(a)
+    return resultat
+
+def potens(a,b):
+    resultat = a^b
+    return resultat
+
+def sin(a,b):
+    resultat = math.sin(math.radians(a))
+    return resultat
+
+def cos(a,b):
+    resultat = math.cos(math.radians(a))
+    return resultat
+
+def tan(a,b):
+    resultat = math.tan(math.radians(a))
+    return resultat
