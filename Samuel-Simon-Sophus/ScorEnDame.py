74f05a9904d85dacfbc95ec0913483eb97af0f3f
BuildTools <unconfigured@null.spigotmc.org>
2019-05-16 13:12:02 +0200

    Fixede alt Version 2.0

--- Samuel-Simon-Sophus/ScorEnDame.py
+++ Samuel-Simon-Sophus/ScorEnDame.py
@@ -6,14 +6,19 @@ from dagenEfter import *
 
 
 def spil():
-    print("Du åbner døren ind til den lokale bar og kigger rundt. Du ser den mest sexede kvinde sidde ovre i kanten af baren. Det tager dig lidt tid men du samler modet og går hen til hende.")
+    print("""
+    Du åbner døren ind til den lokale bar og kigger rundt. Du ser den mest sexede kvinde sidde ovre i kanten af baren. Det tager dig lidt tid men du samler modet og går hen til hende.""")
     svar = paaBaren()
 
     if (svar == "3"):
         sex = hjemmeHosHende()
+    else:
+        print("Tak fordi du spillede.")
 
         if (sex == 1):
             dagenEfter()
+        else:
+            print("Tak fordi du spillede.")
 
 
 spil()
--- Samuel-Simon-Sophus/hjemmeHosHende.py
+++ Samuel-Simon-Sophus/hjemmeHosHende.py
@@ -1,22 +1,25 @@
 def hjemmeHosHende():
     sex = 0
-    hvadnu = input(
-        """Du klarede det, du charmerede dig ind på damen og har fået lov til at komme hjem til hende. Men du er ikke færdig endnu, du vil helst godt i seng med hende og måske få en date mere.
+    hvadnu = input("""
+    Du klarede det, du charmerede dig ind på damen og har fået lov til at komme hjem til hende. Men du er ikke færdig endnu, du vil helst godt i seng med hende og måske få en date mere.
         
-        Hvad vil du nu?
-        1) for at gå ud i køkkenet
-        2) for at gå direkte til soveværelset
-        3) for at gå på toilettet
+    Hvad vil du nu?
+    1) for at gå ud i køkkenet
+    2) for at gå direkte til soveværelset
+    3) for at gå på toilettet
 
-        Dit valg: """)
+    Dit valg: """)
 
     if (hvadnu == "1"):
-        print("Du valgte at gå ud i køkkenet, hvor du æder alt hendes mad og bliver smidt ud. Dårlig stil...")
+        print("""
+        Du valgte at gå ud i køkkenet, hvor du æder alt hendes mad og bliver smidt ud. Dårlig stil...
+        """)
 
     if (hvadnu == "2"):
-        print("""Du går ud til pigen på hendes soveværelse, det er relativt lille men der er plads til to i sengen. 
-Det er ikke specielt romantisk men i hygger jer og du overnatter.
-""")
+        print("""
+        Du går ud til pigen på hendes soveværelse, det er relativt lille men der er plads til to i sengen. 
+        Det er ikke specielt romantisk men i hygger jer og du overnatter.
+        """)
         sex = 1
 
     if (hvadnu == "3"):
@@ -29,18 +32,21 @@ Det er ikke specielt romantisk men i hygger jer og du overnatter.
         Dit valg: """)
 
         if (lorteloesning == "1"):
-            print("""Du smed lorten ud af vinduet. Den perfekte løsning. 
-            Du vasker dine hænder og går ind og får dit retmæssige fisse""")
+            print("""
+            Du smed lorten ud af vinduet. Den perfekte løsning. 
+            Du vasker dine hænder og går ind og får dit retmæssige fisse.""")
             sex = 1
             return sex
 
         if (lorteloesning == "2"):
-            print("""Du valgte at lade lorten ligge. Et valg som kune en kryster ville tage. 
-Det viser sig at hun også skal på toilettet bagefter, og du bliver smidt hjem af en fuld pige der synes dit lort er klamt.""")
+            print("""
+            Du valgte at lade lorten ligge. Et valg som kune en kryster ville tage. 
+            Det viser sig at hun også skal på toilettet bagefter, og du bliver smidt hjem af en fuld pige der synes dit lort er klamt.""")
 
         if (lorteloesning == "3"):
-            print("""Du prøver at fikse toilettet, du har jo fikset ting før.
-Desværre er det her et ret kompliceret problem og du ender dækket i lortevand 
-Du siger hvad der er sket og du bliver smidt ud og får sendt en regning på 2000kr. dagen efter.""")
+            print("""
+            Du prøver at fikse toilettet, du har jo fikset ting før.
+            Desværre er det her et ret kompliceret problem og du ender dækket i lortevand.
+            Du siger hvad der er sket og du bliver smidt ud og får sendt en regning på 2000kr. dagen efter.""")
 
     return sex
