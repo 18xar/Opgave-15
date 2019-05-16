72d729cf1734f93ab16700fa16146b389a117f01
Nichi <nichi@suzuki-poulsen.dk>
2019-05-16 14:08:35 +0200

    Større opdatering til main script

--- NinaMohammadNichi/mohammadsMain.py
+++ NinaMohammadNichi/mohammadsMain.py
@@ -2,20 +2,30 @@
 from tkinter import *
 from NinaMohammadNichi import drawPrimitives as dp
 
-# theLabel = Label(root,text="Første vindue")
-#theLabel.pack()
-
-height = 100.0
-length = 100.0
-width = 100.0
-def plzWork ():
-    print("Plz?")
 root = Tk()
+
+theLabel = Label(root,text="Første vindue")
+theLabel.pack()
 
 bottomFrame = Frame(root)
 bottomFrame.pack(side=BOTTOM)
+instruks = Text(root, height=2, width=2)
+instruks.pack()
+instruks.insert('5.0', 'Indtast')
 
-button1 = Button(root, text="Button 1", command=lambda: dp.drawBox(height, length, width))
+hLabel = Label(root, text="Højde")
+hLabel.pack()
+
+height = Entry(root)
+height.pack()
+
+length = Entry(root)
+length.pack()
+
+width = Entry(root)
+width.pack()
+
+button1 = Button(root, text="Button 1", command=lambda: dp.drawBox(int(height), int(length), int(width)))
 button1.pack()
 
-root.mainloop()
+mainloop()
