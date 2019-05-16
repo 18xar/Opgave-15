cdb782682dd99a7e1f82b79757990b1eaaf9288f
SophusCHTX <soph2262@edu.aarhustech.dk>
2019-05-16 11:16:00 +0200

    lavet start

--- Samuel-Simon-Sophus/hjemmeHosHende.py
+++ Samuel-Simon-Sophus/hjemmeHosHende.py
@@ -1,13 +1,26 @@
 def hjemmeHosHende():
+    sex = 0
     hvadnu = input("""Du klarede det, du charmerede dig ind på damen
-                og har fået lov til at komme hjem til hende.
-                Men du er ikke færdig endnu, du vil helst godt i seng med hende og måske få en date mere.
-
-                Hvad vil du nu?
-                Tast 1 for at gå ud i køkkenet
-                Tast 2 for at gå direkte til soveværelset
-                Tast 3 for at gå på toilettet)
-
-                Dit valg: """)
+          og har fået lov til at komme hjem til hende. 
+          Men du er ikke færdig endnu, du vil helst godt i seng med hende og måske få en date mere.
+            Hvad vil du nu?
+          Tast 1 for at gå ud i køkkenet
+          Tast 2 for at gå direkte til soveværelset
+          Tast 3 for at gå på toilettet)
+          
+          Dit valg: """)
     if (hvadnu == "1"):
-        print("...")
+        print("Du valgte at gå ud i køkkenet, hvor du æder alt hendes mad og bliver smidt ud. Dårlig stil")
+    if (hvadnu == "2"):
+        print("Du gå ud til pigen på hendes soveværelse, dete r relativt lille men der er plads til to i sengen. "
+              "Det er ikke specielt romantisk men i hygger jer og du overnatter.")
+        sex = 1
+    if (hvadnu == "3"):
+        lorteloesning = input("du går ud på toilettet fordi du skal skide, du sætter dig på kummen og laver en stor lort. "
+              "Men du kan ikke skylle. Hvad gør du?"
+                             "Tast 1 for at smide lorten ud af vinduet"
+                             "Tast 2 for at lade lorten ligge og tørre dig og gå ind på soveværelset"
+                             "Tast 3 for at prøve at fixe toilettet")
+        if(lorteloesning == "1"):
+            print("Du smed lorten ud af vinduet og vaskede hænder. Den perfekte løsning. "
+                  "Du må bare håbe ingen finder ud af det var dig" )
