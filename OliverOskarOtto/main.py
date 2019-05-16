5a2cc894d709ba0f266ecae1e82db5cd58989c47
oskarkragh <kraghoskar@gmail.com>
2019-05-16 11:12:21 +0200

    arcade kordinat virker

---
+++ OliverOskarOtto/main.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+import PointsToDistance
+import CosSinCalc
+
+layout = [[sg.Text("Point A:")],
+    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+    [sg.Text("Point B:")],
+    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+    [sg.Text("Point C:")],
+    [sg.Text("x = "), sg.InputText(0), sg.Text("y = "), sg.InputText(0)],
+    [sg.Submit()]
+]
+
+done = sg.Window("Put in coordinates").Layout(layout)
+button, values = done.Read()
+
+Ax = float(values[0])
+Ay = float(values[1])
+Bx = float(values[2])
+By = float(values[3])
+Cx = float(values[4])
+Cy = float(values[5])
+
+AB, AC, BC = (PointsToDistance.PointsToDistance(Ax, Ay, Bx, By, Cx, Cy))
+print("Length AB is " + str(AB))
+print("Length AB is " + str(AC))
+print("Length AB is " + str(BC))
+
+CosSinCalc(AB, AC, BC)
