ad1fc033925ba5cb4821100bbfa033b5f071fcbd
OttoJHTX <sejeotto@gmail.com>
2019-05-16 11:13:42 +0200

    Fixet småting

--- OliverOskarOtto/main.py
+++ OliverOskarOtto/main.py
@@ -23,7 +23,7 @@ Cy = float(values[5])
 
 AB, AC, BC = (PointsToDistance.PointsToDistance(Ax, Ay, Bx, By, Cx, Cy))
 print("Length AB is " + str(AB))
-print("Length AB is " + str(AC))
-print("Length AB is " + str(BC))
-
-CosSinCalc(AB, AC, BC)
+print("Length AC is " + str(AC))
+print("Length BC is " + str(BC))
+print("")
+CosSinCalc.CosSinCalc(AB, AC, BC)
