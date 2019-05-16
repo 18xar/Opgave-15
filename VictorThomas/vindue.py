3524c296d80c4cdd03ee579ef9d6a5f565b17395
Thomas <thoaskson@gmail.com>
2019-05-16 11:41:43 +0200

    Bug fix

--- VictorThomas/vindue.py
+++ VictorThomas/vindue.py
@@ -1,5 +1,5 @@
-import arcade
-
+import arcade, random, math
+import
 
 arcade.open_window(1200,600,"vindue")#window
 arcade.set_background_color((255,255,255))#color
@@ -9,5 +9,16 @@ arcade.set_background_color((255,255,255))#color
 
 
 
-arcade.finish_render()
+while (True):
+    startSpeed = random.randint(1, 3)
+    acceleration = 1.01
+    speed = startSpeed * acceleration
+
+    xPos = random.randint(0, 1200, 10)
+
+
+
+
+
+    arcade.finish_render()
 arcade.run()
