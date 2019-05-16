0bdeb358e24b973a993285018c4bd756a483c7dc
IsabellaE <isabella.eriksen@gmail.com>
2019-05-16 12:26:21 +0200

    Indsatte prisme funktion

--- KacperIsabellaEmma/Funtioner.py
+++ KacperIsabellaEmma/Funtioner.py
@@ -9,8 +9,8 @@ def CirkelP():
     b = random.randint(1, 255)
 
     x = random.randint(0, 600)
-    y= random.randint(0,600)
-    radius= random.randint(19,200)
+    y = random.randint(0,600)
+    radius = random.randint(19,200)
 
     arcade.draw_circle_filled(x,y,radius,(r,g,b))
 
