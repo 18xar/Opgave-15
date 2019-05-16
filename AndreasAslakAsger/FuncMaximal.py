78b92a629dc113dfa0fe0846c77bd82184cb62ad
DevilishMutant <andreas.gkp@gmail.com>
2019-05-16 11:24:31 +0200

    1. version

---
+++ AndreasAslakAsger/FuncMaximal.py
@@ -0,0 +1,10 @@
+import math
+
+def Strømstyrke(Ieff):
+    # Ieff er effektivstrømstyrken
+    resultat = math.sqrt(2) * Ieff
+    return resultat
+def Spændingsforskel(Ueff):
+    # Ueff er effektivspændingen
+    resultat = math.sqrt(2) * Ueff
+    return resultat
